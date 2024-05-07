ba22fca478ff6dce80874150b9e893e45955b224
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-05-07 15:10:29 +0300

    add class Tree

--- Tree.h
+++ Tree.h
@@ -1,14 +1,35 @@
 #pragma once
 #include <iostream>
 
+class Node;
+class Tree;
+typedef Node* NodePointer;
+
 using namespace std;
 
 class Node
 {
+public:
 	int value;
 	Node* parent;
 	Node* left;
 	Node* right;
 	// 0 - black | 1 - red 
 	int color;
+};
+
+class Tree
+{
+private:
+	NodePointer root;
+	NodePointer leaf;
+public:
+	Tree()
+	{
+		leaf = new Node();
+		leaf->color = 0;
+		leaf->left = nullptr;
+		leaf->right = nullptr;
+		root = leaf;
+	}
 };
