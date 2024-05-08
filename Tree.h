6b100de63e4dd3c00472881f3340116a0b1e3a68
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-05-09 02:57:52 +0300

    add insert method

--- Tree.h
+++ Tree.h
@@ -90,4 +90,119 @@ public:
 		temp->right = node;
 		node->parent = temp;
 	}
+
+	void fixAfterInsert(NodePointer node)
+	{
+		NodePointer uncle;
+
+		while (node->parent->color == 1)
+		{
+			if (node->parent == node->parent->parent->right)
+			{
+				uncle = node->parent->parent->left;
+				if (uncle->color == 1)
+				{
+					uncle->color = 0;
+					node->parent->color = 0;
+					node->parent->parent->color = 1;
+					node = node->parent->parent;
+				}
+				else
+				{
+					if (node == node->parent->left)
+					{
+						node = node->parent;
+						rightRotation(node);
+					}
+
+					node->parent->color = 0;
+					node->parent->parent->color = 1;
+					leftRotation(node->parent->parent);
+				}
+			}
+			else
+			{
+				uncle = node->parent->parent->right;
+				if (uncle->color == 1)
+				{
+					uncle->color = 0;
+					node->parent->color = 0;
+					node->parent->parent->color = 1;
+					node = node->parent->parent;
+				}
+				else
+				{
+					if (node == node->parent->left)
+					{
+						node = node->parent;
+						leftRotation(node);
+					}
+					node->parent->color = 0;
+					node->parent->parent->color = 1;
+					rightRotation(node->parent->parent);
+				}
+			}
+
+			if (node == root)
+			{
+				break;
+			}
+		}
+
+		root->color = 0;
+	}
+	
+	void insert(int value)
+	{
+		NodePointer node = new Node;
+		NodePointer pointTree = this->root;
+		NodePointer temp = nullptr;
+
+		node->parent = nullptr;
+		node->value = value;
+		node->left = leaf;
+		node->right = leaf;
+		node->color = 1;
+
+		while (pointTree != leaf)
+		{
+			temp = pointTree;
+			if (node->value > pointTree->value)
+			{
+				pointTree = pointTree->right;
+			}
+			else
+			{
+				pointTree = pointTree->left;
+			}
+		}
+
+		node->parent = temp;
+
+		if (temp == nullptr)
+		{
+			root = node;
+		}
+		else if (node->value > temp->value)
+		{
+			temp->right = node;
+		}
+		else
+		{
+			temp->left = node;
+		}
+
+		if (node->parent == nullptr)
+		{
+			node->color = 0;
+			return;
+		}
+
+		if (node->parent->parent == nullptr)
+		{
+			return;
+		}
+
+		fixAfterInsert(node);
+	}
 };
--- main.cpp
+++ main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 
 int main()
 {
-
+	Tree tree;
+	tree.insert(1);
+	tree.insert(23);
+	tree.insert(34);
+	tree.insert(14);
+	tree.insert(3);
 }
