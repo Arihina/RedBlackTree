2d7e192dad65bf72934d6e27c8b98870e3a0e41b
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-05-08 21:51:30 +0300

    add left rotation method

--- Tree.h
+++ Tree.h
@@ -26,10 +26,39 @@ private:
 public:
 	Tree()
 	{
-		leaf = new Node();
+		leaf = new Node;
 		leaf->color = 0;
 		leaf->left = nullptr;
 		leaf->right = nullptr;
 		root = leaf;
+	}
+
+	void leftRotation(NodePointer node)
+	{
+		NodePointer temp = node->right;
+		node->right = node->left;
+
+		if (temp->left != leaf)
+		{
+			temp->left->parent = node;
+		}
+
+		temp->parent = node->parent;
+
+		if (node->parent == nullptr)
+		{
+			this->root = temp;
+		}
+		else if (node = node->parent->left)
+		{
+			node->parent->left = temp;
+		}
+		else
+		{
+			node->parent->right = temp;
+		}
+
+		temp->left = node;
+		node->parent = temp;
 	}
 };
